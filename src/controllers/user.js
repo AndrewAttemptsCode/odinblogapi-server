49bbd1330d45fc93bff043cbe118f0f7989d87fd
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-24 19:22:44 +0100

    feat: add and prepare controllers for routes

---
+++ src/controllers/user.js
@@ -0,0 +1,44 @@
+const prisma = require('../../config/prismaClient');
+const bcrypt = require('bcryptjs');
+
+const getAllUsers = async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.json({ message: 'Users found', users });
+}
+
+const getUser = async (req, res) => {
+  const { userId } = req.params;
+
+  const user = await prisma.user.findUnique({
+    where: { id: Number(userId) },
+  })
+
+  if (user) {
+    res.json({ message: 'User found', user });
+  } else {
+    res.json({ message: 'No user found' });
+  }
+}
+
+const createUser = async (req, res) => {
+  const {username, password, email} = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password: hashedPassword,
+      email,
+    }
+  })
+
+  res.json({ message: 'User created', user });
+}
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  createUser
+}
