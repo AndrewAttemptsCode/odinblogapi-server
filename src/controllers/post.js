10fc582d3748904d424fd2b0e83da60c1ebbfea5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-25 16:20:21 +0100

    feat: add route to create a new post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,10 +1,33 @@
 const prisma = require('../../config/prismaClient');
 
 const getAllPosts = async (req, res) => {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    include: {
+      author: {
+        select: {
+          username: true,
+        }
+      }
+    }
+  });
   res.json({ posts });
-}
+};
+
+const createPost = async (req, res) => {
+  const { title, text } = req.body;
+  const authorId = 1; // when login set up, change to req.user.id
+
+  const post = await prisma.post.create({
+    data: {
+      title,
+      text,
+      authorId,
+    },
+  });
+  res.json({ message: 'Post created', post });
+};
 
 module.exports = {
-  getAllPosts
-}
+  getAllPosts,
+  createPost,
+};
