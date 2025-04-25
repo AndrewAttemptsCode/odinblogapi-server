b5bc7037a405ca9155f67ecb588863b5662fc8bb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-25 15:30:00 +0100

    feat: add auth route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/auth', routes.auth);
 app.use('/users', routes.user);
 app.use('/posts', routes.post);
 app.use('/comments', routes.comment);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+const auth = require('./auth');
 const user = require('./user');
 const post = require('./post');
 const comment = require('./comment');
 
 module.exports = {
+  auth,
   user,
   post,
   comment
