fcd996e1b27276b18baee9cd6ef20e3e6b146f25
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-26 16:25:00 +0100

    feat: add authentication and authorization middleware to applicable routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const controllers = require('../controllers');
+const authenticateUser = require('../middlewares/authenticateUser');
+const authorizeAdmin = require('../middlewares/authorizeAdmin');
 
 const router = Router();
 
 router.get('/', controllers.post.getAllPosts);
-router.post('/', controllers.post.createPost);
+router.post('/', authenticateUser, authorizeAdmin, controllers.post.createPost);
 router.get('/:postId', controllers.post.getPost);
-router.put('/:postId', controllers.post.publishPost);
-router.post('/:postId/comments', controllers.post.createComment);
+router.put('/:postId', authenticateUser, authorizeAdmin, controllers.post.publishPost);
+router.post('/:postId/comments', authenticateUser, controllers.post.createComment);
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const controllers = require('../controllers');
+const authenticateUser = require('../middlewares/authenticateUser');
+const authorizeAdmin = require('../middlewares/authorizeAdmin');
 
 const router = Router();
 
-router.get('/', controllers.user.getAllUsers);
+router.get('/', authenticateUser, authorizeAdmin, controllers.user.getAllUsers);
 router.post('/', controllers.user.createUser);
-router.get('/:userId', controllers.user.getUser);
+router.get('/:userId', authenticateUser, authorizeAdmin, controllers.user.getUser);
 
 module.exports = router;
