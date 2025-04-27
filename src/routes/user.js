bc2333fcb9ccb5f337be092e247f0686c7b21399
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-27 18:17:09 +0100

    refactor: add register validation to the create user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,16 @@ const { Router } = require('express');
 const controllers = require('../controllers');
 const authenticateUser = require('../middlewares/authenticateUser');
 const authorizeAdmin = require('../middlewares/authorizeAdmin');
+const registerValidation = require('../middlewares/registerValidation');
 
 const router = Router();
 
 router.get('/', authenticateUser, authorizeAdmin, controllers.user.getAllUsers);
-router.post('/', controllers.user.createUser);
+router.post('/', registerValidation, controllers.user.createUser);
 router.get('/:userId', authenticateUser, authorizeAdmin, controllers.user.getUser);
 
 module.exports = router;
+
+// TODO
+// comment validation
+// post validation
