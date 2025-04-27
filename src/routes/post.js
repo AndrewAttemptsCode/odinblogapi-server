8400fea513a12594d158d72ffce15da5cdd229c2
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-27 19:45:16 +0100

    refactor: add post validation to the create post route

--- src/routes/post.js
+++ src/routes/post.js
@@ -3,13 +3,14 @@ const controllers = require('../controllers');
 const authenticateUser = require('../middlewares/authenticateUser');
 const authorizeAdmin = require('../middlewares/authorizeAdmin');
 const commentValidation = require('../middlewares/commentValidation');
+const postValidation = require('../middlewares/postValidation');
 
 const router = Router();
 
 router.get('/', controllers.post.getAllPosts);
-router.post('/', authenticateUser, authorizeAdmin, controllers.post.createPost);
+router.post('/', authenticateUser, authorizeAdmin, postValidation, controllers.post.createPost);
 router.get('/:postId', controllers.post.getPost);
 router.put('/:postId', authenticateUser, authorizeAdmin, controllers.post.publishPost);
-router.post('/:postId/comments', commentValidation, authenticateUser, controllers.post.createComment);
+router.post('/:postId/comments', authenticateUser, commentValidation, controllers.post.createComment);
 
 module.exports = router;
