c54beb78d8ebb5e4963f1b9aa7fc5491c5ba2cab
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-27 18:16:42 +0100

    feat: add validation and sanitization for registering an account

---
+++ src/middlewares/registerValidation.js
@@ -0,0 +1,55 @@
+const { body } = require('express-validator');
+const prisma = require('../../config/prismaClient');
+
+const registerValidation = [
+  body('username')
+    .trim()
+    .isLength({ min: 3 }).withMessage('Minimum length of 3 characters')
+    .isLength({ max: 20 }).withMessage('Maximum length of 20 characters')
+    .matches(/^[a-zA-Z0-9]+$/).withMessage('Letters and numbers only')
+    .custom(async (username) => {
+      try {
+        const userExists = await prisma.user.findUnique({
+          where: { username },
+        });
+        if (userExists) {
+          throw new Error('Username already exists');
+        }
+        return true;
+      } catch (error) {
+        throw new Error('Database query failed. Please try again later.')
+      }
+    }),
+
+  body('email')
+    .trim()
+    .isEmail().withMessage('Invalid Email syntax')
+    .custom(async (email) => {
+      try {
+        const existingEmail = await prisma.user.findUnique({
+          where: { email },
+        });
+        if (existingEmail) {
+          throw new Error('Email already exists');
+        }
+        return true;
+      } catch (error) {
+        throw new Error('Database query failed. Please try again later.')
+      }      
+    }),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 }).withMessage('Minimum length of 8 characters'),
+
+  body('confirmPassword')
+    .trim()
+    .custom((confirmPassword, { req }) => {
+      if (confirmPassword !== req.body.password) {
+        throw new Error('Passswords do not match');
+      }
+      return true;
+    }),
+];
+
+module.exports = registerValidation;
