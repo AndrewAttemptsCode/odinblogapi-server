4d8552658c10929a3cfe7152f348c534f2c5c93e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-27 18:44:56 +0100

    refactor: update route for leaving a comment with comment validation middleware

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const controllers = require('../controllers');
 const authenticateUser = require('../middlewares/authenticateUser');
 const authorizeAdmin = require('../middlewares/authorizeAdmin');
+const commentValidation = require('../middlewares/commentValidation');
 
 const router = Router();
 
@@ -9,6 +10,6 @@ router.get('/', controllers.post.getAllPosts);
 router.post('/', authenticateUser, authorizeAdmin, controllers.post.createPost);
 router.get('/:postId', controllers.post.getPost);
 router.put('/:postId', authenticateUser, authorizeAdmin, controllers.post.publishPost);
-router.post('/:postId/comments', authenticateUser, controllers.post.createComment);
+router.post('/:postId/comments', commentValidation, authenticateUser, controllers.post.createComment);
 
 module.exports = router;
