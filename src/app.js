8b5c10f1869bf6dec9365a109235b573eaa15070
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-28 18:52:20 +0100

    feat: add cors middleware for front end to access the endpoints

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static('public'));
 app.use(express.json());
