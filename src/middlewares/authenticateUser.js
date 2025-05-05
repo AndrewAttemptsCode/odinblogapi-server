6eb6eaae4dd18164f537614fea2867568bfa73b7
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-05 18:37:14 +0100

    refactor: allow custom response so that it returns json rather than the default text response

--- src/middlewares/authenticateUser.js
+++ src/middlewares/authenticateUser.js
@@ -1,5 +1,19 @@
 const passport = require('../../config/passport');
 
-const authenticateUser = passport.authenticate('jwt', { session: false });
+const authenticateUser = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info ) => {
+    if (err || !user) {
+      return res.status(401).json({
+        errors: [{ 
+          path: 'auth',
+          msg: 'You must be logged in to leave a comment.',
+         }],
+      });
+    };
+    
+    req.user = user;
+    next();
+  })(req, res, next);
+};
 
 module.exports = authenticateUser;
