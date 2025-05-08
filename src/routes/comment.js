476fa44abf4e4f9de9e2ab60dc7b4e5a60d4bc3c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-08 16:46:32 +0100

    feat: add route to delete a comment by the comment id

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -6,5 +6,6 @@ const controllers = require('../controllers/index');
 const router = Router();
 
 router.get('/', authenticateUser, authorizeAdmin, controllers.comment.getComments);
+router.delete('/:commentId', authenticateUser, authorizeAdmin, controllers.comment.removeComment);
 
 module.exports = router;
