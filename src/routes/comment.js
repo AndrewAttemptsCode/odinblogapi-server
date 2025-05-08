42eb996019f7dfba5e10acfd4e6baa403a7f5475
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-08 15:25:54 +0100

    feat: add route to get all comments and the author username tied to the comment

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const authenticateUser = require('../middlewares/authenticateUser');
+const authorizeAdmin = require('../middlewares/authorizeAdmin');
+const controllers = require('../controllers/index');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Comments route' });
-})
+router.get('/', authenticateUser, authorizeAdmin, controllers.comment.getComments);
 
 module.exports = router;
